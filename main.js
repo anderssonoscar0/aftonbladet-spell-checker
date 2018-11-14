7315df607efd3fe41960e219118eb6b3fc9c2a75
Oscar Andersson <anderssonoscar0@gmail.com>
2018-11-14 22:45:45 +0100

    Add word cleaner, Add Spellchecker

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 let Parser = require('rss-parser');
 let parser = new Parser();
+var SpellChecker = require('simple-spellchecker');
 
 const fetch = require('node-fetch');
 const config = require('./config.js');
@@ -10,6 +11,7 @@ const config = require('./config.js');
 // Discord startup
 client.on('ready', () => {
   console.log('Startup Sucess!');
+  readRRS();
 });
 client.login(config.discordToken);
 
@@ -36,21 +38,39 @@ function readRRS () {
     let feed = await parser.parseURL('https://www.aftonbladet.se/nyheter/rss.xml');
     feed.items.forEach(item => {
       const string = item.link;
-      var articleId = string.substr(0, string.lastIndexOf('/')).substr(33);
+      let articleId = string.substr(0, string.lastIndexOf('/')).substr(33);
       fetch(config.aftonbladetBaseUrl + articleId)
         .then(res => res.text())
         .then(htmlbody => {
-          var parsedBody = HTMLParser.parse(htmlbody);
-          var authorName = parsedBody.querySelector('._3ij4i').rawText.toLowerCase().replace(' ', '.');
-          var authorEmail = authorName === 'tt' ? 'webbnyheter@aftonbladet.se' : authorName + '@aftonbladet.se'; // If authorName 'TT' -> newsroom is the author
-          console.log(authorName + ' : ' + authorEmail);
+          let parsedBody = HTMLParser.parse(htmlbody);
+          const authorName = parsedBody.querySelector('._3ij4i').rawText.toLowerCase().replace(' ', '.');
+          const authorEmail = authorName === 'tt' ? 'webbnyheter@aftonbladet.se' : authorName + '@aftonbladet.se'; // If authorName 'TT' -> newsroom is the author
+
+          let articleBody = parsedBody.querySelector('._3p4DP._1lEgk').rawText.replace(/\./g, ' ');
+          checkSpelling(articleBody, authorEmail);
         });
     });
   })();
 }
 
-function checkSpelling (html) {
+function checkSpelling (html, authorEmail) {
   console.log('running check');
+  let wordArray = html.split(' ');
+  for (var i = 0; i < wordArray.length; i++) {
+    const cleanedWord = cleanWord(wordArray[i]);
+    console.log(cleanedWord);
+  }
+}
+
+function cleanWord (word) {
+  let cleanedWord = word;
+  cleanedWord = cleanedWord.replace('+', '');
+  cleanedWord = cleanedWord.replace('-', '');
+  cleanedWord = cleanedWord.replace('?', '');
+  cleanedWord = cleanedWord.replace('FÖLJ', '');
+  cleanedWord = cleanedWord.replace('”', '');
+  cleanedWord = cleanedWord.replace('!', '');
+  return cleanedWord;
 }
 
 function alertError (t) {
