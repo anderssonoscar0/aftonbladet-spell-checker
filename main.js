2a3b62b968cdbaf7181fdc5568f22234172db326
Oscar Andersson <anderssonoscar0@gmail.com>
2018-11-14 20:42:21 +0100

    Get Author name from each article

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var HTMLParser = require('node-html-parser');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 let Parser = require('rss-parser');
@@ -8,10 +9,16 @@ const config = require('./config.js');
 
 (async () => {
   let feed = await parser.parseURL('https://www.aftonbladet.se/nyheter/rss.xml');
-  console.log(feed.title);
   feed.items.forEach(item => {
     const string = item.link;
     var articleId = string.substr(0, string.lastIndexOf('/')).substr(33);
     console.log(articleId);
+    fetch(config.aftonbladetBaseUrl + articleId)
+      .then(res => res.text())
+      .then(htmlbody => {
+        var parsedBody = HTMLParser.parse(htmlbody);
+        var authorName = parsedBody.querySelector('._3ij4i').rawText;
+        console.log(authorName);
+      });
   });
 })();
