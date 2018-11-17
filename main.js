5f4b627263607a2cff11f37ec8a03080131fa522
Oscar Andersson <anderssonoscar0@gmail.com>
2018-11-17 20:10:55 +0100

    Add word to dictionary from discord

--- main.js
+++ main.js
@@ -47,8 +47,16 @@ client.on('message', message => {
     message.channel.send('Denying!');
   }
 
-  if (command === 'add') {
-    addWordToDictionary(args[0]);
+  if (command === 'addword') {
+    const invalidChars = /[ a-z!•►”–@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    if (args.length < 2) {
+      message.channel.send('Missing argument');
+    } else if (invalidChars.test(args[0])) {
+      message.channel.send('Argument contains invalid characters. Command is ".addword <Number> <ArticleId>"');
+    } else {
+      addWordToDictionary(args);
+    }
+   
   }
 });
 
@@ -133,15 +141,41 @@ function addNewArticle (words, sentences, articleId, authorEmail) {
   newArticle.save();
 }
 
-function addWordToDictionary (word) {
+function addWordToDictionary (args) {
   // Adding word to Dictionary
-  try {
-    fs.appendFileSync('./dict/sv-SE.dic', '\n' + word);
-  } catch (err) {
-    /* Handle the error */
-    throw err;
-  }
-  normalize();
+  const wordSpotInArray = parseInt(args[0]);
+  const articleId = args[1];
+  
+  Article.findOne({ '_id': articleId }, function (err, doc) {
+    if (err) throw err;
+    let words = [];
+    let sentences = [];
+    console.log(doc.words.length)
+    for (var i = 0; i < doc.words.length; i++) {
+      console.log(doc.words[i]);
+      console.log(i + ' vs ' + wordSpotInArray);
+      if (wordSpotInArray === i) {
+        // Word to be added to dictionary. SKIPPING
+        try {
+          fs.appendFileSync('./dict/sv-SE.dic', '\n' + doc.words[i]);
+        } catch (err) {
+          /* Handle the error */
+          throw err;
+        }
+        normalize();
+        client.channels.get(config.discordChannelId).send(doc.words[i] + ' was added to the dictionary.');
+      } else {
+        words.push(doc.words[i]);
+        sentences.push(doc.sentences[i]);
+      }
+    }
+    doc.words = words;
+    doc.sentences = sentences;
+    console.log(doc.words.length)
+    doc.alerted = false;
+    doc.save();
+    alertSchedule();
+  });
 }
 
 function normalize () {
