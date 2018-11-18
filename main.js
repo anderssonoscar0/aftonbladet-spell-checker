8acb03299b3d9f7e324cff195c817270faf6a43c
Oscar Andersson <anderssonoscar0@gmail.com>
2018-11-18 14:14:58 +0100

    Remove unnessecary escapes (semistandard errors)

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ client.on('message', message => {
   }
 
   if (command === 'addword') {
-    const invalidChars = /[ a-z!✓•▪►”–@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    const invalidChars = /[ a-z!✓•▪►”–@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/;
     if (args.length < 2) {
       message.channel.send('Missing argument');
     } else if (invalidChars.test(args[0])) {
@@ -98,7 +98,7 @@ function checkSpelling (html, authorEmail, articleId) {
         wordArray[i].toUpperCase() + ' ' + wordArray[i + 1] + ' ' + wordArray[i + 2] + ' ' + wordArray[i + 3];
 
         // Check if the sentence contains invalid characters
-        const invalidChars = /[!•►✓▪”–@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?1234567890]/;
+        const invalidChars = /[!•►✓▪”–@#$%^&*()_+\-=[\]{};':"\\|,.<>/?1234567890]/;
         if (invalidChars.test(sentence)) {
           // Sentence contains invalid characters. SKIPPING
         } else {
@@ -113,7 +113,7 @@ function checkSpelling (html, authorEmail, articleId) {
 }
 
 function cleanWord (word) {
-  const invalidChars = /[ A-ZÅÄÖ!✓▪•►”–@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?1234567890]/;
+  const invalidChars = /[ A-ZÅÄÖ!✓▪•►”–@#$%^&*()_+\-=[\]{};':"\\|,.<>/?1234567890]/;
   if (invalidChars.test(word) || word === '') {
     return undefined; // The word contains invalid characters, returning undefined and skipping it later.
   } else {
