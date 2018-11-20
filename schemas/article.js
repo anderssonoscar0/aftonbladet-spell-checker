37d13abeba93bf1fa2a461f0a9453ce641564342
Oscar Andersson <anderssonoscar0@gmail.com>
2018-11-20 22:58:36 +0100

    Remove unused database column

--- schemas/article.js
+++ schemas/article.js
@@ -5,7 +5,6 @@ var newArticleSchema = new Schema({
   _id: { type: String, unique: true },
   words: { type: Array },
   sentences: { type: Array },
-  alerted: { type: Boolean, default: false },
   authorEmail: { type: String },
   date: { type: Date, default: Date.now },
   discordMessageId: { type: String }
