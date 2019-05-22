cb083237c253aca07522e41aa3a361ec082f07bb
Oscar Andersson <anderssonoscar0@gmail.com>
2019-05-22 17:07:19 +0200

    Update schedule timer

--- main.js
+++ main.js
@@ -372,7 +372,7 @@ function sendDiscordVote (args, message) {
 }
 
 // Scheudule article search every 5 minutes
-schedule.scheduleJob('*/3 * * * *', function () {
+schedule.scheduleJob('*/10 * * * *', function () {
   logger.log('(SCHEDULE-JOB) - Running RRS reader')
   readRRS()
 })
